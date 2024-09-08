e0ed1708cbe6a5866f20a0af5364dd40c2245791
143062 <162887638+143062@users.noreply.github.com>
2024-09-09 00:51:17 +0200

    Buttons now match the style of app

--- public/views/change_picture_modal.php
+++ public/views/change_picture_modal.php
@@ -1,12 +1,12 @@
 <div id="profile-form-modal" class="profile-form-modal">
     <form id="profile-form" class="profile-form" enctype="multipart/form-data">
         <span class="back-button"><img src="/public/img/arrow_back.svg" alt="Back" class="back-icon" id="close-profile-form-modal"></span>
-        <h2>Zmień zdjęcie</h2>
+        <h2>Zdjęcie profilowe</h2>
         <label for="file-input" class="file-input-label">Wybierz plik</label>
         <input type="file" id="file-input" class="file-input" name="profile_picture" accept="image/*">
         <p id="file-name" class="file-name"></p>
         <div class="form-buttons">
-            <button type="submit" id="upload-button">Zmień</button>
+            <button type="submit" id="upload-button">Zmień zdjęcie</button>
         </div>
     </form>
 </div>
