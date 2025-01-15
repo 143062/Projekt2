46061ad27da5fc76db769bc0525d946d956fe0e4
143062 <162887638+143062@users.noreply.github.com>
2025-01-15 01:48:49 +0100

    Final fix
    sync with local

---
+++ public/views/add_note_modal.php
@@ -0,0 +1,15 @@
+<div id="note-form-container" class="note-form-container">
+    <div class="note-form">
+        <input type="text" id="note-title" class="note-title" placeholder="Tytuł">
+        <div class="share-section">
+            <div id="shared-with" class="shared-with"></div>
+            <button id="add-friend" class="add-friend">
+                <img src="/public/img/plus.svg" alt="Dodaj znajomego">
+            </button>
+        </div>
+        <textarea id="note-content" class="note-content" placeholder="Treść notatki"></textarea>
+        <button id="save-note" class="save-note">
+            <img src="/public/img/check.svg" alt="Zapisz">
+        </button>
+    </div>
+</div>
